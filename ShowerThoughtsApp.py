552f428c1351f0ddc9658359bcfbd2f875b0a45c
Abdullah Saleh M Alawfi <aalawfi@email.sc.edu>
2021-12-25 21:39:04 -0500

    Switched form writing component in app to to writing in .kv

--- ShowerThoughtsApp.py
+++ ShowerThoughtsApp.py
@@ -2,7 +2,6 @@ import kivy
 from kivy.app import App
 # TODO: FLoatLayout or GridLayout ? 
 from kivy.uix.gridlayout import GridLayout
-
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
@@ -12,19 +11,6 @@ from kivy.uix.textinput import TextInput
 class Layout (GridLayout):
     def __init__(self, **kwargs): 
         super(Layout, self).__init__(**kwargs)
-        # determine the number of columns of the layout
-        self.cols = 1
-        # Header widget (maybe use an image instead ??  )
-        self.add_widget( Label(text="Shower Thoughts") )
-        # Inputs 
-        self.add_widget( Label(text="First name") )
-        self.add_widget( TextInput(multiline =False,size_hint = (0.2,  None) ) )
-
-        self.add_widget( Label(text="Last name") )
-        self.add_widget( TextInput(multiline =False,size_hint = (0.2,  None) ) )
-
-        self.add_widget( Label(text="Phone number ") )
-        self.add_widget( TextInput(text="+1",multiline =False,size_hint = (0.2,  None) ) )
 
 class ShowerThoughtsApp(App):
    def build(self):
